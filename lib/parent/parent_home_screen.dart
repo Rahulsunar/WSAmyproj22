28f372bde6fedc2b7cfe98cd541091c419148ddc
Rahul sunar <101958367+Rahulsunar@users.noreply.github.com>
2024-09-13 18:35:44 +0545

    showing guardian specific child
    showing guardian specific child

--- lib/parent/parent_home_screen.dart
+++ lib/parent/parent_home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:women_safety_app/utils/constants.dart';
 
 class ParentHomeScreen extends StatelessWidget {
   const ParentHomeScreen({super.key});
@@ -6,9 +9,42 @@ class ParentHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('parent screen'),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.pink,
+          // backgroundColor: Color.fromARGB(255, 250, 163, 192),
+          title: Text("SELECT CHILD"),
+        ),
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('users')
+              .where('type', isEqualTo: 'child')
+              .where('parentEmail',
+                  isEqualTo: FirebaseAuth.instance.currentUser!.email)
+              .snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) {
+              return Center(child: progressIndicator(context));
+            }
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (BuildContext context, int index) {
+                final d = snapshot.data!.docs[index];
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    color: Color.fromARGB(255, 250, 163, 192),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                        title: Text(d['name']),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ));
   }
 }
