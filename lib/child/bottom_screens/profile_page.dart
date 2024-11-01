3743537b952eed039ebfa23e260216f5f13fa6fc
Rahul sunar <101958367+Rahulsunar@users.noreply.github.com>
2024-11-01 14:51:06 +0545

    Review page
    Review page,and profile page update also

--- lib/child/bottom_screens/profile_page.dart
+++ lib/child/bottom_screens/profile_page.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:uuid/uuid.dart';
 import 'package:women_safety_app/child/child_login_screen.dart';
 import 'package:women_safety_app/components/PrimaryButton.dart';
 import 'package:women_safety_app/components/custom_textfield.dart';
@@ -15,75 +21,155 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  TextEditingController nameC=TextEditingController();
-  final key= GlobalKey<FormState>();
+  TextEditingController nameC = TextEditingController();
+  final key = GlobalKey<FormState>();
   String? id;
-  getName()async{
-    await FirebaseFirestore.instance.collection('users')
-    .where('id',isEqualTo: FirebaseAuth.instance.currentUser!.uid)
-    .get()
-    .then((value) {
-      nameC.text= value.docs.first['name'];
-      id=value.docs.first.id;
+  String? profilePic;
+  String? downloadUrl;
+  bool IsSaving = false;
+  getData() async {
+    await FirebaseFirestore.instance
+        .collection('users')
+        .where('id', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+        .get()
+        .then((value) {
+      setState(() {
+        nameC.text = value.docs.first['name'];
+        id = value.docs.first.id;
+        profilePic = value.docs.first['profilePic'];
+      });
     });
   }
 
-@override
+  @override
   void initState() {
     super.initState();
-    getName();
+    getData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(18.0),
-          child: Center(
-            child: Form(
-              key:key,
-              child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text("UPDATE YOUR PROFILE", 
-                style: TextStyle(fontSize: 25),
+      body: IsSaving == true
+          ? Center(
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.pink,
+              ),
+            )
+          : SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Center(
+                  child: Form(
+                      key: key,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            "UPDATE YOUR PROFILE",
+                            style: TextStyle(fontSize: 25),
+                          ),
+                          SizedBox(height: 15),
+                          GestureDetector(
+                            onTap: () async {
+                              final XFile? pickImage = await ImagePicker()
+                                  .pickImage(
+                                      source: ImageSource.gallery,
+                                      imageQuality: 50);
+                              if (pickImage != null) {
+                                setState(() {
+                                  profilePic = pickImage.path;
+                                });
+                              }
+                            },
+                            child: Container(
+                              child: profilePic == null
+                                  ? CircleAvatar(
+                                      backgroundColor: Colors.deepPurple,
+                                      radius: 80,
+                                      child: Center(
+                                          child: Image.asset(
+                                        'assets/add_pic.png',
+                                        height: 80,
+                                        width: 80,
+                                      )))
+                                  : profilePic!.contains('http')
+                                      ? CircleAvatar(
+                                          backgroundColor: Colors.deepPurple,
+                                          radius: 80,
+                                          backgroundImage:
+                                              NetworkImage(profilePic!),
+                                        )
+                                      : CircleAvatar(
+                                          backgroundColor: Colors.deepPurple,
+                                          radius: 80,
+                                          backgroundImage:
+                                              FileImage(File(profilePic!))),
+                            ),
+                          ),
+                          CustomTextfield(
+                            controller: nameC,
+                            hintText: nameC.text,
+                            validate: (v) {
+                              if (v!.isEmpty) {
+                                return 'please enter your updated name';
+                              }
+                              return null;
+                            },
+                          ),
+                          SizedBox(height: 25),
+                          Primarybutton(
+                              title: "UPDATE",
+                              onPressed: () async {
+                                if (key.currentState!.validate()) {
+                                  SystemChannels.textInput
+                                      .invokeMethod('TextInput.hide');
+                                  profilePic == null
+                                      ? Fluttertoast.showToast(
+                                          msg: 'please select profile picture')
+                                      : update();
+                                }
+                              })
+                        ],
+                      )),
                 ),
-                SizedBox(height: 15),
-                CircleAvatar(
-                  radius: 40 ,
-                  child: Center(child: Image.asset(
-                    'assets/add_pic.png',
-                  height: 35,
-                  width: 35,
-                  ))
-                ),
-               CustomTextfield(
-                controller: nameC,
-                hintText: nameC.text,
-                validate: (v){
-                  if(v!.isEmpty){
-                   return 'please enter your updated name'; 
-                  }
-                  return null;
-                },
-               ),
-               SizedBox(height: 25),
-               Primarybutton(title: "UPDATE", onPressed: ()async{
-                await FirebaseFirestore.instance
-                .collection('users')
-                .doc(id)
-                .update({
-                  'name': nameC.text,
-                }).then((value)=>
-                Fluttertoast.showToast(msg: 'name upadated successfully')
-                );
-               })
-            ],
-            )),
-          ),
-        ),
-      ),
+              ),
+            ),
     );
+  }
+
+  Future<String?> uploadImage(String filePath) async {
+    try {
+      final fileName = Uuid().v4();
+      final Reference fbStorage =
+          FirebaseStorage.instance.ref('profile').child(fileName);
+
+      final UploadTask uploadTask = fbStorage.putFile(File(filePath));
+      await uploadTask.then((p0) async {
+        downloadUrl = await fbStorage.getDownloadURL();
+      });
+      return downloadUrl;
+    } catch (e) {
+      Fluttertoast.showToast(msg: e.toString());
+    }
+  }
+
+  update() async {
+    setState(() {
+      IsSaving = true;
+    });
+    uploadImage(profilePic!).then((value) {
+      Map<String, dynamic> data = {
+        'name': nameC.text,
+        'profilePic': downloadUrl,
+      };
+      FirebaseFirestore.instance
+          .collection('users')
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .update(data);
+      setState(() {
+        IsSaving = false;
+      });
+    });
   }
 }
--- lib/child/bottom_screens/review_page.dart
+++ lib/child/bottom_screens/review_page.dart
@@ -1,11 +1,143 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class ReviewPage extends StatelessWidget {
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:women_safety_app/components/PrimaryButton.dart';
+import 'package:women_safety_app/components/SecondaryButton.dart';
+import 'package:women_safety_app/components/custom_textfield.dart';
+import 'package:women_safety_app/utils/constants.dart';
+
+class ReviewPage extends StatefulWidget {
+  @override
+  State<ReviewPage> createState() => _ReviewPageState();
+}
+
+class _ReviewPageState extends State<ReviewPage> {
+  TextEditingController locationC = TextEditingController();
+  TextEditingController viewsC = TextEditingController();
+  bool isSaving = false;
+  showAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (_) {
+          return AlertDialog(
+            title: Text("Review Your Place"),
+            content: Form(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CustomTextfield(
+                      hintText: 'Enter Location',
+                      controller: locationC,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CustomTextfield(
+                      controller: viewsC,
+                      hintText: 'Enter Location',
+                      maxLines: 3,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            actions: [
+              Primarybutton(
+                  title: "SAVE",
+                  onPressed: () {
+                    saveReview();
+                    Navigator.pop(context);
+                  }),
+              TextButton(
+                  child: Text("Cancel"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  })
+            ],
+          );
+        });
+  }
+
+  saveReview() async {
+    setState(() {
+      isSaving = true;
+    });
+    await FirebaseFirestore.instance.collection('reviews').add({
+      'location': locationC.text,
+      'views': viewsC.text,
+    }).then((value) {
+      setState(() {
+        isSaving = false;
+        Fluttertoast.showToast(msg: 'Review uploaded successfully');
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Scaffold(
-        body: Text("Rating Page"),
+    return Scaffold(
+      body: isSaving == true
+          ? Center(child: CircularProgressIndicator())
+          : SafeArea(
+              child: Column(
+                children: [
+                  Text(
+                    "Recent Review by others",
+                    style: TextStyle(fontSize: 25, color: Colors.black),
+                  ),
+                  Expanded(
+                      child: StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection('reviews')
+                        .snapshots(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting &&
+                          !snapshot.hasData) {
+                        // Show the loading indicator only if it's the first load and there is no data yet
+                        return Center(child: CircularProgressIndicator());
+                      }
+
+                      if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                        // Show a message if there are no reviews
+                        return Center(child: Text("No reviews available."));
+                      }
+
+                      return ListView.builder(
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final data = snapshot.data!.docs[index];
+                          return Padding(
+                            padding: const EdgeInsets.all(3.0),
+                            child: Card(
+                              elevation: 10,
+                              child: ListTile(
+                                title: Text(
+                                  data['location'],
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.black),
+                                ),
+                                subtitle: Text(data['views']),
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  )),
+                ],
+              ),
+            ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.pink,
+        onPressed: () {
+          showAlert(context);
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
